e215e4251c18fda0ec4bd19f9b2cacaa878a5530
Ryan Fowler <ryan.fowler@singlewire.com>
2016-04-01 10:12:23 -0500

    change group/version for singlewire release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject herolabs/apns "0.5.1"
+(defproject singlewire/apns "0.5.2"
   :description "A simple lightweight library to use with the Apple push notification service."
   :url "https://github.com/HEROLABS/herolabs-apns"
   :license {:name "Eclipse Public License - v 1.0"
